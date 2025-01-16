12ea519dd45ce50e8ce1d054fec06ef38d8336b6
Stefan Walter <woigl@mac.com>
2025-01-16 10:36:09 +0400

    fix: corrected tsup global name

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm', 'iife'],
-  globalName: 'RSQLQueryBuilder',
+  globalName: 'RSQLBuilder',
   dts: true,
   sourcemap: true,
   clean: true,
